071a18d9eb15f22f72fd859484066d8b01ac425d
Sepehr Akbari <isepehrakbari@gmail.com>
2024-12-05 23:14:06 -0600

    updated Src

--- test.R
+++
@@ -1,8 +0,0 @@
-library(spotifyr)
-
-Sys.setenv(SPOTIFY_CLIENT_ID = '16c72804f9f34b97a90b932586dda1f4')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '22b6f9cb82c04c9392b4df57e6030f89')
-
-access_token <- get_spotify_access_token()
-
-beatles <- get_artist_audio_features('the beatles')
